15079a59f5e791dec4cd8a3f87cad7f4dca3d63f
Robert Wallis <robert@walnutcube.com>
2021-06-17 12:26:40 -0700

    Initial working demo

---
+++ ClockOnTop Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Clock On Top
+//
+//  Copyright (C) 2021 Robert A. Wallis, All Rights Reserved.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet var window: ClockPanel!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ ClockOnTop Mac/ClockPanel.swift
@@ -0,0 +1,33 @@
+//
+//  ClockPanel.swift
+//  Clock On Top
+//
+//  Copyright (C) 2021 Robert A. Wallis, All Rights Reserved.
+//
+
+import Cocoa
+
+class ClockPanel: NSPanel {
+
+	@IBOutlet weak var timeLabel: NSTextField!
+	var refreshTimer: Timer?
+
+	override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
+		super.init(contentRect: contentRect, styleMask: style, backing: backingStoreType, defer: flag)
+
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateFormat = "HH:mm:ss"
+
+		refreshTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: {_ in
+
+			let time = dateFormatter.string(from: Date())
+			self.timeLabel?.stringValue = time
+
+		})
+	}
+
+	override func setIsVisible(_ flag: Bool) {
+		refreshTimer?.fire()
+		self.setIsVisible(flag)
+	}
+}
